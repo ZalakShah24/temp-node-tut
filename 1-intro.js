c4f3a66de3b57707b11ef41f40a34f987a3975a4
ZalakShah24 <zalak.s@ahduni.edu.in>
2022-08-19 23:06:26 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,16 @@
+
+
+
+
+const amount = 1 
+
+if(amount<10){
+    console.log('small number')
+}
+else{
+    console.log('large number')
+}
+
+console.log(`My 1st node app`)
+
+// npm - node package manager
